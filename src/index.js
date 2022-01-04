f4c3c42bd5b8e026aa0d61114bba4a268e8b6437
Anton Shipitsin <antonshipitsin@gmail.com>
2022-01-05 00:20:10 +0600

    feat: add function to check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, arr) {
+    let newArr = []
+    arr.forEach(e => {
+        let h = e.join('')
+        newArr.push(h)
+    })
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < newArr.length; j++) {
+            if (str.indexOf(newArr[j]) != -1) {
+                str = str.replace(newArr[j], '')
+                i--
+                // console.log(str)
+            }
+        }
+    }
+   return str.length == 0
+
 }
